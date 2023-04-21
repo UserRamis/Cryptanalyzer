293339a6e4a84a980898bbbc96d9c664d714194d
UserRamis <ramis2003@mail.ru>
2023-04-21 12:23:44 +0300

    Changed Bruteforce

--- src/main/java/com/javarush/cryptanalyser/garipov/Bruteforce.java
+++ src/main/java/com/javarush/cryptanalyser/garipov/Bruteforce.java
@@ -1,53 +1,40 @@
 package com.javarush.cryptanalyser.garipov;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Bruteforce
 {
-    File file;
-    File boot = new File("Bruteforce.txt");
+    private File file;
+    private  File boot = new File("Bruteforce.txt");
+    private  String s="^([а-яА-Я-]+(\\s|,\\s|:\\s|;\\s)){2,}";
+    private Decryption decryptionforbruteforce=new Decryption();
+    private  Pattern pattern = Pattern.compile(s);
+    private StringBuilder stringBuilder = new StringBuilder();
+    private String line;
 
     public Bruteforce(File file)
     {
         this.file=file;
     }
-    Decryption decryptionforbruteforce=new Decryption();
 
-
-    public File bruteforceOperation() throws IOException
-    {
-        boot.createNewFile();
-        boolean flag=false;
-        int key=0;
-
-        boot= decryptionforbruteforce.decryptionOperation(file,key,boot);
-        while (!flag)
+    public File bruteforceOperation() throws IOException {
+        for (int i = 1; i < 80; i++)
         {
-            boot= decryptionforbruteforce.decryptionOperation(file,key,boot);
-
-
-            String str1 = Files.readString((Path.of(boot.toURI())));
-            StringBuilder str= new StringBuilder(str1);
-
-            if(doesNotContainLongWords(str)&&!doesNotHavePreposition(str)&&!doesNotHaveEnoughSpaces(str))
-            {
-                return boot;
-
+            boot=clearFileContent(boot);
+            boot = decryptionforbruteforce.decryptionOperation(file, i, boot);
+            BufferedReader reader = new BufferedReader(new FileReader(boot));
+            stringBuilder.delete(0,stringBuilder.length());
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
             }
-            else
+            Matcher matcher = pattern.matcher(stringBuilder);
+            if(matcher.find())
             {
-                boot=clearFileContent(boot);
-                key++;
-                str.setLength(0);
-                //str1=" ";
-
+                break;
             }
-
         }
         return boot;
     }
-
     public File  clearFileContent(File file) {
         try {
             FileWriter fileWriter = new FileWriter(file);
@@ -58,54 +45,4 @@ public class Bruteforce
         }
         return file;
     }
-
-
-    private boolean doesNotContainLongWords(StringBuilder decryptedText) {
-        String[] array = decryptedText.toString().split(" ");
-        for (String s : array) {
-            if (s.length() > 30) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean doesNotHavePreposition(StringBuilder decryptedText) {
-        String[] array = decryptedText.toString().split(" ");
-        int count = 0;
-        for (String s : array) {
-            if (s.length() == 1) {
-                count++;
-            }
-        }
-        return count == 0;
-    }
-
-    private boolean doesNotHaveEnoughSpaces(StringBuilder decryptedText) {
-        String[] array = decryptedText.toString().split("\n");
-        int count = 0;
-        for (String s : array) {
-            if (s.contains(" ")) {
-                String[] arr = s.split(" ");
-                count += arr.length - 1;
-            }
-        }
-        return count < array.length;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
